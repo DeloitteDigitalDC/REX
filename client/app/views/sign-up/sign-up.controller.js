203feee601ead7ca5bbd0a4e00e89dc391d6e79c
lomelchenko <lomelchenko@deloitte.com>
2015-06-22 09:43:23 -0400

    sets up sign up

---
+++ client/app/views/sign-up/sign-up.controller.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc controller
+ *
+ * @name rex.controller:SignUp
+ *
+ * @description
+ * Controller for rex
+ */
+(function() {
+
+  angular
+    .module('rex')
+    .controller('SignUpCtrl', SignUpCtrl);
+
+  function SignUpCtrl() {
+    var vm = this;
+
+  }
+
+})();
+
