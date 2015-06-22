3d3ea2fa3554aceadfc883b51cebef73ae7d223f
Danny Blue <dblue@deloitte.com>
2015-06-22 08:17:20 -0400

    Fix server side tests and code coverage. Update matching to exclude .spec.js files in report

--- server/api/fda/fda.spec.js
+++ server/api/fda/fda.spec.js
@@ -1,25 +1,25 @@
 'use strict';
+/* istanbul ignore */
+var assert  = require('assert'),
+    request = require('supertest'),
+    app     = require('../../app');
 
-//var assert  = require('assert'),
-//    request = require('supertest'),
-//    app     = require('../../app');
-//
-//describe('GET /fda/drug/event.json', function () {
-//
-//  it('should respond with JSON array', function (done) {
-//    //request(app)
-//    //  .get('/fda/drug/event.json')
-//    //  .expect('Content-Type', /json/)
-//    //  .expect(200)
-//    //  .end(function (err, res) {
-//    //    if (err) {
-//    //      return done(err);
-//    //    }
-//    //
-//    //    assert(res.body.results.length > 0);
-//    //
-//    //    done();
-//    //  });
-//  });
-//
-//});
+describe('GET /fda/drug/event.json', function () {
+
+  it('should respond with JSON array', function (done) {
+    request(app)
+      .get('/fda/drug/event.json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        assert(res.body.results.length > 0);
+
+        done();
+      });
+  });
+
+});
