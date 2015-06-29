65542ab01925898c5a4060971c9e06a2f461f294
lomelchenko <lomelchenko@deloitte.com>
2015-06-29 16:01:15 -0400

    cleanup

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -4,46 +4,11 @@
 var fb      = require('../../firebase'),
     request = require('request'),
     config  = require('../../config'),
-    md5     = require('MD5'),
     url     = config.firebase,
     db      = require('../../db');
 
 
 var user = {};
-
-/**
- * @memberof user.controller
- *
- * @description
- * login through firebase
- */
-//user.login = function (req, res) {
-//  var opts = {
-//    email   : req.body.username,
-//    password: req.body.password
-//  };
-//
-//  fb.login(opts, __success, __error);
-//
-//  function __success(auth) {
-//    request(url + 'users/' + auth.uid + '/.json?auth=' + auth.token, function (err, reslt, body) {
-//      try {
-//        auth.data = JSON.parse(body);
-//      }
-//      catch (e) {
-//        auth.data = body;
-//      }
-//
-//      auth.success = 'LOGGED_IN';
-//
-//      res.send(auth);
-//    });
-//  }
-//
-//  function __error(err) {
-//    res.send(err);
-//  }
-//};
 
 /**
  * get the details for the authenticated used;
@@ -92,52 +57,7 @@ user.getDetails = function (req, res) {
 //  request.patch(config.firebase + '/users/' + req.params.uid + '.json?auth=' + req.cookies.token, {json: req.body}).pipe(res);
 //};
 
-/**
- * @name createUser
- *
- * @memberof user.controller
- *
- * @description
- * create a new user
- */
-//user.createUser = function (req, res) {
-//  var data = req.body;
-//
-//  var user = {
-//    email   : data.username,
-//    password: data.password
-//  };
-//
-//  var details = {
-//    nickName    : data.firstName,
-//    email       : data.username,
-//    gravatarHash: md5(data.username.toLowerCase())
-//  };
-//
-//  // create user
-//  fb.ref.createUser(user, function (err, userData) {
-//    if (err) {
-//      return res.send(err);
-//    }
-//
-//    // if creation is successful log the use in
-//    fb.login(user, __success, __error);
-//
-//    // on successful login update the current users data in the users collection
-//    function __success(authData) {
-//      request.put(config.firebase + '/users/' + userData.uid + '.json?auth=' + authData.token, {json: details}, function (err, data, body) {
-//        authData.success = 'USER_CREATED';
-//        authData.data    = body;
-//
-//        res.send(authData);
-//      });
-//    }
-//
-//    function __error(error) {
-//      res.send(error);
-//    }
-//  });
-//};
+
 
 /**
  * get cabinet drugs
