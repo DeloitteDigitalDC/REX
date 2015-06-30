a256c5d0297f21f330c6d6d67df36185266495c9
Mark Hoffmeyer <mhoffmeyer@gmail.com>
2015-06-30 12:20:38 -0400

    Update run_rex script to include sqlite location

--- deploy/run_rex.sh
+++ deploy/run_rex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #run the application
-PORT=$port FDA_KEY=$fda_key FIREBASE=$firebase node server/app-server.js
+PORT=$port FDA_KEY=$fda_key FIREBASE=$firebase SQLITELOCATION=$sqlitelocation node server/app-server.js
