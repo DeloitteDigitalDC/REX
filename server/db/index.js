516972f0213d8e48c06dc115982ad3fbf2093a07
lomelchenko <lomelchenko@deloitte.com>
2015-06-29 21:46:46 -0400

    fix sql lite reference

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,19 @@
 'use strict';
 
-//todo: no need for self calling function
-module.exports = (function() {
+//FIXME: SQL injection security
+var sqlite3 = require('sqlite3'),
+    db      = new sqlite3.Database(__dirname + '/database.sqlite3');
 
-  //DC Created with: CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "username" TEXT, "password" TEXT, "salt" TEXT, "nickName" TEXT, "gravatarHash" TEXT, "pregnant" BOOLEAN);
-  //Drugs table Created with: CREATE TABLE "drugs" ("db_id" INTEGER PRIMARY KEY AUTOINCREMENT, "username" TEXT, "name" TEXT, "id" TEXT);
+db.run('select * from users', function(err){
+  if(err){
+    db.run('CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "username" TEXT, "password" TEXT, "salt" TEXT, "nickName" TEXT, "gravatarHash" TEXT, "pregnant" BOOLEAN)');
+  }
+});
 
-  //FIXME: SQL injection security
-  var sqlite3 = require('sqlite3'),
-      db      = new sqlite3.Database('./server/db/database.sqlite3');
+db.run('select * from drugs', function(err){
+  if(err){
+    db.run('CREATE TABLE "drugs" ("db_id" INTEGER PRIMARY KEY AUTOINCREMENT, "username" TEXT, "name" TEXT, "id" TEXT)');
+  }
+});
 
-  return db;
-})();
+module.exports = db;
