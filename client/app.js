e9850b54ea56fca98403309a67b0d7f7051f52ca
Danny Blue <dblue@deloitte.com>
2015-06-30 13:05:16 -0400

    fix styles for pregnancy alert on mobile

--- client/app.js
+++ client/app.js
@@ -43,18 +43,18 @@ angular
     return {
       response     : function (response) {
         if (response.status === 401) {
-          console.log("Response 401");
+          console.log('Response 401');
         }
         return response;
       },
       responseError: function (rejection) {
         if (rejection.status === 401) {
-          console.log("Response Error 401", rejection);
+          console.log('Response Error 401', rejection);
           $cookies.remove('uid');
           $location.path('/');
         }
         return $q.reject(rejection);
       }
-    }
+    };
   }
 })();
--- client/app/views/drug-profile/drug-profile.controller.js
+++ client/app/views/drug-profile/drug-profile.controller.js
@@ -29,7 +29,7 @@
 
     vm.addCabinetDrug = addCabinetDrug;
     vm.checkCabinet = checkCabinet;
-    vm.editCabinetDrug = editCabinetDrug;
+    vm.removeCabinetDrug = removeCabinetDrug;
 
     init();
 
@@ -83,8 +83,14 @@
      *
      * @return {*}
      */
-    function editCabinetDrug(evt) {
-      var modal = modals.editDrug(evt);
+    function removeCabinetDrug(evt) {
+      var modal = modals.removeDrug(evt);
+
+      modal.then(function () {
+        removeDrug();
+
+        $state.go('main.cabinet');
+      });
 
       return modal;
     }
@@ -100,6 +106,17 @@
       user.addCabinetDrug(evt, drug, function () {
         checkCabinet();
       });
+    }
+
+    /**
+     * Remove a drug from your drug cabinet
+     *
+     * @memberof DrugProfileCtrl
+     */
+    function removeDrug() {
+       user.deleteCabinetDrug(vm.id, function (){
+         checkCabinet();
+       });
     }
 
     /**
